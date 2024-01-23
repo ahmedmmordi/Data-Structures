d01e6301d7bc75ea97f9bc4774078bfc73dbf58f
ahmed <ahmed.m.hamada2003@gmail.com>
2024-01-23 13:03:25 +0200

    Provided a better code and explanation.

--- Non Primitive/Linear/8- Queue/Linked-List-based Queue/LinkedListQueue.cpp
+++ Non Primitive/Linear/8- Queue/Linked-List-based Queue/LinkedListQueue.cpp
@@ -1,205 +1,159 @@
 #include <iostream>
-#include <cassert>
-#define endl '\n'
+#define endl "\n"
 using namespace std;
 
-class LinkedList {
+class Queue {
     private:
-        // Private class to represent a node in the linked list
         class Node {
             public:
                 int data;
                 Node *next;
-
                 // Parametrized constructor
                 Node(int value) {
                     this->data = value;
                     this->next = nullptr;
                 }
         };
-
-        Node *head;
-        Node *tail;
-        int length;
+        Node *Front;
+        Node *Rear;
+        int size;
 
     public:
-        // Default constructor
-        LinkedList() {
-            head = nullptr;
-            tail = nullptr;
-            length = 0;
+        // default constructor
+        Queue() {
+            this->Front = nullptr;
+            this->Rear = nullptr;
+            this->size = 0;
         }
 
         // Destructor to release memory
-        ~LinkedList() {
-            while (head!=nullptr) {
-                Node *temp = head;
-                head = head->next;
+        ~Queue() {
+            while (Front!=nullptr) {
+                Node *temp = Front;
+                Front = Front->next;
                 delete temp;
             }
+            Front = nullptr;
+            Rear = nullptr;
+            size = 0;
         }
 
-        // Function to print the elements of the linked list
-        void Print_List() {
-            Node *now = head;
-            while (now!=nullptr) {
-                cout << now->data << ' ';
-                now = now->next;
-            }
-            cout << endl;
-        }
-
-        // Function to insert a new node at the back of the linked list
-        void Insert_Back(int value) {
-            Node *new_node = new Node(value);
-            if (head==nullptr) {
-                head = new_node;
-                tail = new_node;
-            }
-            else {
-                tail->next = new_node;
-                tail = new_node;
-            }
-            ++length;
-        }
-
-        // Function to search for a value in the linked list
-        int Search(int value) {
-            int position = 0;
-            for (Node *now=head;now;now=now->next,++position) {
-                if (now->data==value) {
-                    return position;
-                }
-            }
-            return -1; // Value not found
-        }
-
-        // Function to delete the a specific node (1-Based) from the linked list
-        void Delete_Node(int index) {
-            --index;
-            assert(index>=0 and index<length);
-            if (index==0) {
-                Node *temp = head;
-                head = head->next;
-                delete temp;
-                --length;
-                if (head==nullptr) {
-                    tail = nullptr;
-                }
-            }
-            else {
-                int con = 1;
-                Node *Previous = head;
-                while (con<index) {
-                    Previous = Previous->next;
-                    ++con;
-                }
-                Node *temp = Previous->next;
-                Previous->next = temp->next;
-                delete temp;
-                --length;
-                if (Previous->next==nullptr) {
-                    tail = Previous;
-                }
-            }
-        }
-
-        // Function to check if the linked list is empty
-        bool Is_Empty() {
-            return head==nullptr;
-        }
-
-        // Function to get the length of the linked list
-        int Get_Length() {
-            return length;
-        }
-
-        // Function to get the head node of the linked list
-        Node *Get_Head() {
-            return head;
-        }
-
-        // Function to get the tail node of the linked list
-        Node *Get_Tail() {
-            return tail;
-        }
-};
-
-class Queue {
-    private:
-        LinkedList list; // Queue implemented based on a linked list (as its underlying data structure).
-
-    public:
-        // Function to add an element to the back of the queue.
+        // Function to add an element to the back of the queue
         void Enqueue(int value) {
-            list.Insert_Back(value);
+            Node *new_element = new Node(value);
+            if (Front==nullptr) {
+                Front = new_element;
+                Rear = new_element;
+            }
+            else {
+                Rear->next = new_element;
+                Rear = new_element;
+            }
+            ++size;
         }
 
-        // Function to remove the first (front) element from the queue.
+        // Function to remove the first (front) element from the queue
         void Dequeue() {
-            return list.Delete_Node(1); // First (front) node 1-Based
+            if (Front==nullptr) throw logic_error("Cannot delete from already empty queue.\n");
+            else {
+                Node *Temp = Front;
+                Front = Front->next;
+                delete Temp;
+                --size;
+            }
         }
 
-        // Function to get the value of the front element in the queue.
-        int Get_Front() {
-            return list.Get_Head()->data;
+        // Function to get the value of the first element in the queue
+        int GetFront() {
+            if (Front==nullptr) throw logic_error("Queue is empty.\n");
+            else return Front->data;
         }
 
-        // Function to get the value of the back element in the queue.
-        int Get_Back() {
-            return list.Get_Tail()->data;
+        // Function to get the value of the last element in the queue
+        int GetBack() {
+            if (Rear==nullptr) throw logic_error("Queue is empty.\n");
+            else  return Rear->data;
         }
 
-        // Function to check if the queue is empty.
-        bool Is_Empty() {
-            return list.Is_Empty();
+        // Function to search about an element in the queue
+        bool IsExist(int value) {
+            if (Front==nullptr) throw logic_error("Queue is empty.\n");
+            else {
+                Node *Temp = Front;
+                while(Temp!=nullptr) {
+                    if (Temp->data==value) return true; // found
+                    Temp = Temp->next;
+                }
+                return false; // not found
+            }
         }
 
-        // Function to get the current size of the queue.
-        int Get_Size() {
-            return list.Get_Length();
+        // Function to check if the queue is empty
+        bool IsEmpty() {
+            return Front==nullptr;
         }
 
-        // Function to check if a specific value exists in the queue.
-        int Is_Exist(int value) {
-            return list.Search(value);
+        // Function to return the number of elements
+        int Size() {
+            return size;
         }
 
-        // Function to print the elements of the queue.
-        void Print() {
-            list.Print_List();
+        // Function to print the elements of the queue
+        void print() {
+            if (Front==nullptr) throw logic_error("Cannot print an empty queue.\n");
+            else{
+                cout << "List: ";
+                Node *Temp = Front;
+                while(Temp!=nullptr) {
+                    cout << Temp->data << ' ';
+                    Temp = Temp->next;
+                }
+                cout << endl;
+            }
+        }
+
+        // Function to remove all the elements of the queue
+        void Clear() {
+            if (Front==nullptr) throw logic_error("Cannot clear an already empty queue.\n");
+            else {
+                while(Front!=nullptr) {
+                    Node *Temp = Front;
+                    Front = Front->next;
+                    delete Temp;
+                }
+                Front = nullptr;
+                Rear = nullptr;
+                size = 0;
+            }
         }
 };
 
 int main() {
 
-    Queue My;
-    cout<< (My.Is_Empty() ? "Empty" : "Not empty") << endl;
-    cout << "Size is: " << My.Get_Size() << endl;
-    My.Enqueue(2);
-    My.Enqueue(5);
-    My.Enqueue(7);
-    My.Enqueue(12);
-    cout << "Elements are: ";
-    My.Print();
-    cout << "Front is: " << My.Get_Front() << endl;
-    cout << "Back is: " << My.Get_Back() << endl;
+    Queue my;
+    cout << (my.IsEmpty()? "Empty" : "Not empty") << endl;
+    // my.Clear(); // It will throw an exception because its already empty
+    my.Enqueue(1);
+    my.Enqueue(2);
+    my.Enqueue(3);
+    my.Enqueue(4);
+    my.Enqueue(5);
+    cout << "Size: " << my.Size() << endl;
+    my.print();
+    cout << (my.IsExist(3)? "Found" : "Not Found") << endl;
+    cout << (my.IsExist(7)? "Found" : "Not Found") << endl;
+    cout << "Front: " << my.GetFront() << endl;
+    cout << "Back: " << my.GetBack() << endl;
+    my.Dequeue();
+    my.Dequeue();
+    cout << "Size became: " << my.Size() << endl;
+    my.print();
+    my.Clear();
+    cout << "Size now: " << my.Size() << endl;
+    // my.print(); // It will throw an exception because there are no elements to print
 
-    cout << "-----------------------------------------\n";
-
-    cout<< (My.Is_Empty() ? "Empty" : "Not empty") << endl;
-    My.Dequeue();
-    cout << "Elements are: ";
-    My.Print();
-    cout << "Size is: " << My.Get_Size() << endl;
-    cout << "Front is: " << My.Get_Front() << endl;
-    cout << "Back is: " << My.Get_Back() << endl;
-    cout << (My.Is_Exist(6)==-1 ? "Not found" : "Found") << endl; // -1 means not found
-
-    int position = My.Is_Exist(7);
-    if (position==-1) cout << "Not found";
-    else cout << "Found in: " << ++position << endl; // ++ to return it 1-Based
-    cout << "-----------------------------------------\n";
+    my.Enqueue(1);
+    my.print();
 
     return 0;
 }
--- Non Primitive/Linear/8- Queue/Linked-List-based Queue/README.md
+++ Non Primitive/Linear/8- Queue/Linked-List-based Queue/README.md
@@ -1,46 +1,62 @@
 # Linked-List-Based Queue Class
 
-The "Linked-List-Based Queue Class" is an implementation of a queue data structure that relies on a linked list as its underlying data structure. This class provides a convenient and efficient way to manage a queue of elements, allowing you to add elements to the back of the queue, remove elements from the front, and perform various operations on the elements within the queue.
+This is a C++ class, `Queue`, that represents a queue using a linked list. This class uses a private inner class, `Node`, to manage the elements in the queue. The class provides basic methods for queue operations.
 
-## Note
-This `Queue` class is implemented based on a linked list. It is essential to understand how linked lists work before using this class effectively. Linked lists are a data structure where each element (node) holds a value and a reference (or pointer) to the next element in the list. The `Queue` class takes advantage of this linked list structure to provide queue operations.
+## Node Class
+
+The `Node` class is a private inner class within the `Queue`. It is used to represent individual elements in the queue. The `Node` class has the following attributes:
+
+- `int data`: This attribute stores the data associated with the node.
+- `Node *next`: A pointer to the next node in the queue.
+
+The `Node` class features a constructor to initialize a node with a specified value. It sets the `data` attribute to the given value and the `next` pointer to `nullptr`, indicating that there is no next node initially.
+
+## Queue Attributes
+
+- `Node *Front`: This attribute is a pointer to the front (first) node in the queue, representing the element to be dequeued.
+- `Node *Rear`: It is a pointer to the rear (last) node in the queue, representing the element most recently enqueued.
+- `int size`: It keeps track of the number of elements in the queue.
+
+The queue class is designed to manage a first-in, first-out (FIFO) collection of elements, making it suitable for various applications where this data structure is needed.
+
 
 ## Class Methods
 
-### `void Enqueue(int value)`
+### `Queue()`
+- **Description:** Initializes an empty queue.
 
+### `~Queue()`
+- **Description:** Destructor for the queue, cleans up memory allocated for the queue.
+
+### `void Enqueue(int value)`
 - **Description:** Adds an element to the back of the queue.
 
 ### `void Dequeue()`
-
 - **Description:** Removes the first (front) element from the queue.
 
-### `int Get_Front()`
-
+### `int GetFront()`
 - **Description:** Returns the value of the front element in the queue.
 
-### `int Get_Back()`
-
+### `int GetBack()`
 - **Description:** Returns the value of the back element in the queue.
 
-### `bool Is_Empty()`
+### `bool IsExist(int value)`
+- **Description:** Checks if the specified value exists in the queue and returns `true` if it is found, `false` otherwise.
 
+### `bool IsEmpty()`
 - **Description:** Checks if the queue is empty and returns `true` if it is, `false` otherwise.
 
-### `int Get_Size()`
-
+### `int Size()`
 - **Description:** Returns the current size of the queue.
 
-### `int Is_Exist(int value)`
-
-- **Description:** Checks if a specific value exists in the queue and returns its index or -1 if not found.
-
 ### `void Print()`
-
 - **Description:** Prints the elements of the queue.
+
+### `void Clear()`
+- **Description:** Removes all elements from the queue.
 
 ## Example Usage
 
-In the `main` function, you can find various examples demonstrating how to use the `Queue` class. You can create a queue, enqueue elements, dequeue elements, check the front and back elements, check if the queue is empty, get the current size, search for specific values, and print the elements.
+In the `main` function, you can find various examples demonstrating how to use the `Queue` class. You can create a queue, enqueue elements, dequeue elements, check the front and back elements, check if the queue is empty, get the current size, print the elements, and clear the queue.
 
 These examples will help you understand how to work with the `Queue` class and leverage its functionality for your applications.
